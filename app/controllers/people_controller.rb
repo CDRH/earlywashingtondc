173f69da909532edc846674b5519bdb8b1d78b1d
Jessica Dussault <jdussault4@gmail.com>
2015-06-25 14:35:24 -0500

    Very quick and awful hookup to solr people results

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,11 +5,13 @@ class PeopleController < ApplicationController
 
   def show
     id = params[:id]
+    @person = @@solr.query({:q => "id:#{id}"})[:docs][0]
+
     @docs = @@solr.query({:qfield => "peopleID_ss", :qtext => id})
 
-    rdfquery = SPARQL.parse("#{@@prefixes} SELECT ?rel1 ?name1 WHERE 
+    rdfquery = SPARQL.parse("#{@@prefixes} SELECT ?rel1 ?per1 ?name1 WHERE 
                             { osrdf:#{id} ?rel1 ?per1 . 
-                              ?per1 oscys:fullName ?name1 
+                              ?per1 oscys:fullName ?name1
                             }")
     rdf = @@rdf.query(rdfquery)
     @rdfresults = JSON.parse(rdf.to_json)
