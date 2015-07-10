13af26b34d0fb098dede6652ea4ace424ff2e10b
Karin <promo@nirak.net>
2015-07-10 15:16:49 -0500

    DOWN WITH TURBOLINKS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( reset.css )
 Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( prettyPhoto.css )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
 
 Rails.application.config.assets.precompile += %w( prettyPhoto.js )
 Rails.application.config.assets.precompile += %w( script.js )
