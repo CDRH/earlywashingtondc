22290e2ae13e99b2660efb49af06b2a8f3521845
Karin <promo@nirak.net>
2015-07-16 14:24:44 -0500

    Aesthetic changes to views

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,12 +3,14 @@ module PeopleHelper
   def make_alphabet_menu(alphabet_hash)
     # expects a hash like {"a" => 22, "b" => 131} etc
     res = ""
-    link_array = [link_to("All People", peopleAll_path())]
+    
+    link_array = ["<li>" + link_to("All", peopleAll_path()) + "</li>"]
     alphabet_hash.each do |key, value|
       # TODO figure out what to do with blank strings / missing facet because then the solr query is different
-      link_array << link_to("#{key} (#{value})", peopleAll_path({:letter => key})) if !key.empty?
+      label = "#{key} <span>(#{value})</span>".html_safe
+      link_array << "<li>" + link_to(label, peopleAll_path({:letter => key})) + "</li>" if !key.empty? 
     end
-    res += link_array.join(" | ")
+    res += "<ul class='letter_pagination'>" + link_array.join(" ") + "</ul>"
     return res.html_safe
   end
 
