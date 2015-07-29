1c9765e6af84b3ef52518b317559bfdce67596ff
Jessica Dussault <jdussault4@gmail.com>
2015-07-29 16:27:52 -0500

    Pagination a la etsy Moved several pieces into partials deleted what appeared to be unused documents duplicate no tests, TODO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,56 @@ module ApplicationHelper
                       :onchange => "this.form.submit();")
   end
 
+  def paginator_numbers(total_pages)
+    current_page = params[:page] ? params[:page].to_i : 1
+    html = ""
+    # weed out the first and last page, they'll be handled separately
+    prior_three = (current_page-3..current_page-1).reject { |x| x <= 1 }
+    next_three = (current_page+1..current_page+3).reject { |x| x >= total_pages.to_i }
+
+    # add the first page if you're not on it and add dots if it is far from the other pages
+    if current_page != 1
+      html += "<li>"
+      html += link_to "1", to_page("1") 
+      html += "</li>"
+      html += "<li class='disabled'><a href='#'>...</a></li>" if prior_three.min != 2
+    end
+
+    # prior three, current page, and next three
+    html += _add_paginator_options(prior_three)
+    html += "<li class='active'>"
+    html += link_to current_page.to_s, to_page(current_page)
+    html += "</li>"
+    html += _add_paginator_options(next_three)
+
+    # add the last page if you're not on it and add dots if it is far from the other pages
+    if current_page != total_pages.to_i
+      html += "<li class='disabled'><a href='#'>...</a></li>" if next_three.max != total_pages.to_i-1
+      html += "<li>"
+      html += link_to total_pages, to_page(total_pages)
+      html += "</li>"
+    end
+
+    return html.html_safe
+  end
+
+  def _add_paginator_options(range)
+    html = ""
+    range.each do |page|
+      html += "<li>"
+      html += link_to page.to_s, to_page(page)
+      html += "</li>"
+    end
+    return html
+  end
+
+  # paginator method
+  def to_page(page)
+    params = reset_params
+    merged = params.merge({:page => page.to_s})
+    return merged
+  end
+
   def reset_params
     new_params = params.clone
     new_params.delete("action")
