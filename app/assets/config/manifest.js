38b4694eaec391d8f10f3c0191110ef17d2520df
Jessica Dussault <jdussault4@gmail.com>
2019-12-02 13:58:51 -0600

    updates asset handling for rails version

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+// Compatible with Sprockets 3.x defaults to avoid breaking existing apps
+// https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
+
+//= link_tree ../images
+//= link application.scss
+//= link application.js
