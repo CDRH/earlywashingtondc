e76dff5303ee7a4c0189193edee418294ecc243d
Greg Tunink <techgique@gmail.com>
2026-08-26 12:29:02 -0500

    Fix reading person name starting with comma
    - Screen reader only "comma" for comma item in alphabet menu
    - Add "comma" to All People h1 if comma chosen
    - DRY up code for alphabet menu and All People h1
      - Skip to next key if empty right away rather than build label then
        not append a link
      - If no letter chosen on All People, h1 as "All # people" and avoid
        duplicated trailing ifs for "starts with" text
    - Capitalize letters

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -6,17 +6,20 @@ module PeopleHelper
     
     link_array = ["<li>" + link_to("All", peopleAll_path()) + "</li>"]
     alphabet_hash.each do |key, value|
-      # TODO figure out what to do with blank strings / missing facet because then the solr query is different
-      if value != 1
-        label = <<~HTML.html_safe
-          #{key} <span class="count">(#{value}<span class="sr-only-clip"> people</span>)</span>
-        HTML
-      else
-        label = <<~HTML.html_safe
-          #{key} <span class="count">(#{value}<span class="sr-only-clip"> person</span>)</span>
-        HTML
+      next if key.empty?
+      label = "#{key.capitalize} ".html_safe
+      if key == ","
+        label << '<span class="sr-only-clip">comma </span>'.html_safe
       end
-      link_array << "<li>" + link_to(label, peopleAll_path({:letter => key})) + "</li>" if !key.empty? 
+      label << <<~HTML.html_safe
+        <span class="count">#{value}<span class="sr-only-clip">
+      HTML
+      if value != 1
+        label << " people".html_safe
+      else
+        label << " person".html_safe
+      end
+      link_array << "<li>" + link_to(label, peopleAll_path({:letter => key})) + "</li>"
     end
     res += "<ul class='letter_pagination'>" + link_array.join(" ") + "</ul>"
     return res.html_safe
